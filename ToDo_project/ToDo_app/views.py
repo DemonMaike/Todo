76c9360857a4cfbca7b1d5e50701d154ace36c27
DemonMaike <dmitry.malyshevv@ya.ru>
2022-12-21 01:57:00 +0400

    Created a new project and created a user regiser form

---
+++ ToDo_project/ToDo_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request,'ToDo_app/signup.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+            return HttpResponse(f"User {request.POST['username']} created ")
+        else:
+            return HttpResponse('Your passwords is not similar ')
