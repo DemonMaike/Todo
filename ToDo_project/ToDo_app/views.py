131533157acdeafd77266bc50a5788b594d6cc27
DemonMaike <dmitry.malyshevv@ya.ru>
2022-12-21 04:22:42 +0400

    Corrected errors for register, create a currentuser page

--- ToDo_project/ToDo_app/views.py
+++ ToDo_project/ToDo_app/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 def signup(request):
     if request.method == 'GET':
         return render(request,'ToDo_app/signup.html', {'form':UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
-            return HttpResponse(f"User {request.POST['username']} created ")
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currentuser')
+            except IntegrityError:
+                return render(request,'ToDo_app/signup.html', {'form':UserCreationForm(), 'error': 'You alredy register, please, login.'})    
         else:
-            return HttpResponse('Your passwords is not similar ')
+            return render(request,'ToDo_app/signup.html', {'form':UserCreationForm(), 'error': 'Your passwords is not similars, please try again.'})
+
+def home(request):
+    return render(request, 'ToDo_app/home.html')
+
+def currentuser(request):
+    return render(request, 'ToDo_app/current_user.html')
